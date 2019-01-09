65a0a5e4c215744001bb10731f6549e8f1348b21
Thun An de Freitas Chiu <tafc@intranet.pitang.com>
2019-01-09 12:16:02 -0300

    Commit inicial

---
+++ ControleEstoqueEstudo/Controllers/OperacaoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoqueEstudo.Controllers
+{
+    public class OperacaoController : Controller
+    {
+        // GET: Operacao
+        public ActionResult EntradaEstoque()
+        {
+            return View();
+        }
+        public ActionResult SaidaEstoque()
+        {
+            return View();
+        }
+        public ActionResult IndLancPerdaProdutoex()
+        {
+            return View();
+        }
+        public ActionResult Inventario()
+        {
+            return View();
+        }
+    }
+}
